31bbedfdf6a7e20e52e7dc36f04eb420040acf36
Kaarel Kivistik <kaarel.kivistik@sifr.ai>
2019-06-17 13:59:51 +0300

    show a list of smoothies

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from "react";
 import { fetchFromCatalogApi } from "./shared";
 
 const App = () => {
-  const [smoothies, setSmoothies] = useState();
+  const [smoothies, setSmoothies] = useState([]);
 
   useEffect(() => {
-    getSmoothies().then(response => setSmoothies(response));
+    getSmoothies().then(response => setSmoothies(response.content));
   }, []);
 
-  return <pre>{JSON.stringify(smoothies, null, 2)}</pre>;
+  return (
+    <ul>
+      {smoothies.map(smoothie => (
+        <Smoothie key={smoothie.id} smoothie={smoothie} />
+      ))}
+    </ul>
+  );
 };
 
 const getSmoothies = () => fetchFromCatalogApi("/smoothies");
+
+const Smoothie = ({ smoothie }) => <li>{smoothie.name}</li>;
 
 export default App;
